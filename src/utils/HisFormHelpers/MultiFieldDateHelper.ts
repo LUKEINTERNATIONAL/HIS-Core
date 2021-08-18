2fe1b89e8dec47bed6329e0425513a5d58ca61ba
Andrew Mfune <andiemfune@gmail.com>
2021-08-18 10:19:21 +0200

    Refactored how we treat unknown and estimate date values

--- src/utils/HisFormHelpers/MultiFieldDateHelper.ts
+++ src/utils/HisFormHelpers/MultiFieldDateHelper.ts
@@ -1,6 +1,7 @@
 import { FieldType } from "@/components/Forms/BaseFormElements"
 import MonthOptions from "@/components/FormElements/Presets/MonthOptions"
 import { Field, Option } from "@/components/Forms/FieldInterface"
+import Validation from "@/components/Forms/validations/StandardValidations"
 import HisDate from "@/utils/Date"
 
 export interface DateFieldInterface {
@@ -10,22 +11,60 @@ export interface DateFieldInterface {
     validation?: Function;
     computeValue: Function;
     appearInSummary?: Function;
+    allowEstimationField?: boolean;
+    allowUnknown?: boolean;
 }
 
-function onValidation(field: DateFieldInterface, val: Option, formData: any, computedFormData: any) {
-    let data = val
-    if (data) {
-        if (data.value === 'Unknown') return null
+function onValidation(
+    datePart: 'year' | 'month' | 'day', 
+    field: DateFieldInterface, 
+    val: Option, 
+    formData: any, 
+    computedFormData: any) {
 
-        const year = formData[`year_${field.id}`] || {}
-        const month = formData[`month_${field.id}`] || {}
-        const day = formData[`day_${field.id}`] || {}
-        data = {
-            label: val.label,
-            value: HisDate.stitchDate(year.value, month.value, day.value)
-        }
+    const validate = (data: any) => field.validation ? field.validation(data, formData, computedFormData) : true
+
+    if (!val) {
+        // Let any custom validation deal with null values
+        return validate(val)
     }
-    return field.validation ? field.validation(data, formData, computedFormData) : true
+
+    if (val.value.toString().match(/unknown/i)) {
+        if ('allowUnknown' in field && !field.allowUnknown) {
+            return ['Unknown value not permitted']
+        }
+        //If year is unknown, treat the whole date as unknown and dont do anything here
+        if (datePart === 'year') return null
+    }
+    const getValue = (type: string) => {
+        const id = type === 'day' ? field.id : `${type}_${field.id}`
+        return formData[id] ? formData[id].value : ''
+    }
+    const year = getValue('year')
+    let month = ''
+    let day = ''
+    /**
+     * Check date part and clear it's successor's value. 
+     * This will allow us to validate current datePart individually
+    */
+    switch(datePart) {
+        case 'year':
+            month = ''
+            day = ''
+            break
+        case 'month':
+            month = getValue('month')
+            day = ''
+            break
+        case 'day':
+            month = getValue('month')
+            day = getValue('day')
+            break
+    }
+    return validate({
+        label: val.label,
+        value: HisDate.stitchDate(year, month, day)
+    })
 }
 
 function onCondition(field: DateFieldInterface, formData: any) {
@@ -35,30 +74,32 @@ function onCondition(field: DateFieldInterface, formData: any) {
     return field.condition ? field.condition(formData): true 
 }
 
-export function generateDateFields(field: DateFieldInterface, currentDate: string, estimateUnknown=false): Array<Field>{
+export function generateDateFields(field: DateFieldInterface, currentDate=''): Array<Field>{
     const yearId = `year_${field.id}`
     const monthId = `month_${field.id}`
+    const canShowEstimateField = field.allowEstimationField ? field.allowEstimationField : false
+
     return [
         {
             id: yearId,
-            helpText: `Year ${field.helpText}`,
+            helpText: `${field.helpText} Year`,
             type: FieldType.TT_NUMBER,
             appearInSummary: () => false,
             condition: (f: any) => field.condition ? field.condition(f) : true,
-            validation: (v: Option, f: any, c: any) => onValidation(field, v, f, c)
+            validation: (v: Option, f: any, c: any) => onValidation('year', field, v, f, c)
         },
         {
             id: monthId,
-            helpText: `Month ${field.helpText}`,
+            helpText: `${field.helpText} Month`,
             type: FieldType.TT_SELECT,
             appearInSummary: () => false,
             options: () => MonthOptions,
             condition: (f: any) => onCondition(field, f),
-            validation: (v: Option, f: any, c: any) => onValidation(field, v, f, c)
+            validation: (v: Option, f: any, c: any) => onValidation('month', field, v, f, c)
         },
         {
             id: field.id,
-            helpText: `Day ${field.helpText}`,
+            helpText: `${field.helpText} Day`,
             type: FieldType.TT_MONTHLY_DAYS,
             condition: (f: any) => onCondition(field, f),
             appearInSummary: (f: any) => {
@@ -68,7 +109,7 @@ export function generateDateFields(field: DateFieldInterface, currentDate: strin
                 label: `${field.helpText} Date`,
                 value: `${computed.date} ${computed.isEstimate ? '(Estimated Date)': ''}`
             }),
-            validation: (v: Option, f: any, c: any) => onValidation(field, v, f, c),
+            validation: (v: Option, f: any, c: any) => onValidation('day', field, v, f, c),
             computedValue: ({ value }: Option, f: Record<string, any>) => {
                 const day = value
                 let isEstimate = false
@@ -85,13 +126,14 @@ export function generateDateFields(field: DateFieldInterface, currentDate: strin
             id: `estimated_${field.id}`,
             helpText: `${field.helpText} Estimated period`,
             type: FieldType.TT_SELECT,
+            validation: (v: Option) => Validation.required(v),
             summaryMapValue: ({ label }: Option, f: any, computedValue: any) => ({ 
                 label: `${field.helpText} Date Estimate`,
                 value: `${label} (${computedValue.date})`
             }),
             condition: (f: any) => {
                 const conditions = [
-                    estimateUnknown,
+                    canShowEstimateField,
                     f[yearId].value === 'Unknown',
                     field.condition ? field.condition(f): true
                 ]
