381fdc78a81d062860cfe7c835dad1e6885d714a
Andrew Mfune <andiemfune@gmail.com>
2021-08-24 14:37:24 +0200

    Showing patient programs on first page

--- src/views/ProgramManagement.vue
+++ src/views/ProgramManagement.vue
@@ -14,12 +14,15 @@ import { Field } from "@/components/Forms/FieldInterface"
 import { toastWarning, toastSuccess } from "@/utils/Alerts"
 import Validation from "@/components/Forms/validations/StandardValidations"
 import { generateDateFields } from "@/utils/HisFormHelpers/MultiFieldDateHelper"
+import { ProgramService } from "@/services/program_service"
+import { PatientProgramService } from "@/services/patient_program_service"
 import HisStandardForm from "@/components/Forms/HisStandardForm.vue";
 
 export default defineComponent({
     components: { HisStandardForm },
     data: () => ({
         patient: {} as any,
+        patientProgram: {} as any,
         fields: [] as Array<Field>,
         fieldComponent: '' as any,
         programSelectionFieldContext: {} as any,
@@ -29,6 +32,7 @@ export default defineComponent({
             async handler({params}: any) {
                 if (params && params.patient_id) {
                     this.patient = params.patient_id
+                    this.patientProgram = new PatientProgramService(this.patient)
                     this.fields = this.getFields()
                 }
             },
@@ -39,6 +43,14 @@ export default defineComponent({
     methods: {
         async onFinish() {
             // TODO: do something
+        },
+        async patientPrograms() {
+            const programs = await this.patientProgram.getPrograms()
+            return programs.map((p: any) => ({
+                label: p.program.name,
+                value: p.program.program_id,
+                other: { ...p }
+            }))
         },
         programNavButton(name: string, color: string, onClick: Function, fieldIndex=0) {
             return {
@@ -63,7 +75,7 @@ export default defineComponent({
                         this.programSelectionFieldContext = context
                     },
                     validation: (val: any) => Validation.required(val),
-                    options: () => [],
+                    options: () => this.patientPrograms(),
                     config: {
                        hiddenFooterBtns: [
                            'Clear',
