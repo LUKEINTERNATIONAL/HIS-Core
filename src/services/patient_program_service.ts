51e193f853e9594aad7c8199b0c092541107a3dd
Andrew Mfune <andiemfune@gmail.com>
2021-08-24 14:37:03 +0200

    Added patient program helper service

---
+++ src/services/patient_program_service.ts
@@ -0,0 +1,39 @@
+import { ProgramService } from "./program_service";
+
+export class PatientProgramService extends ProgramService {
+    patientId: number
+    programId: number
+    stateId: number
+    patientProgramId: number
+    constructor(patientId: number) {
+        super()
+        this.patientId = patientId
+        this.patientProgramId = -1
+        this.programId = -1
+        this.stateId = -1
+    }
+
+    getPrograms() {
+        return ProgramService.getPatientPrograms(this.patientId)
+    }
+
+    getProgramStates() {
+        return ProgramService.getPatientStates(this.patientId, this.programId)
+    }
+
+    voidProgram(reason: string) {
+        return ProgramService.voidProgram(this.patientProgramId, reason)
+    }
+
+    voidState(reason: string) {
+        return ProgramService.voidState(this.patientId, this.programId, this.stateId, reason)
+    }
+
+    setProgramId(program: number) {
+        this.programId = program
+    }
+
+    setStateId(state: number) {
+        this.stateId = state
+    }
+}
