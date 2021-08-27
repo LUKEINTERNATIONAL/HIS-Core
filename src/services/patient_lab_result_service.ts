a45b4e4a07b7f4d811b3f3b6dd78493681fb44fb
Andrew Mfune <andiemfune@gmail.com>
2021-08-27 10:29:34 +0200

    Initial patient lab result service

---
+++ src/services/patient_lab_result_service.ts
@@ -0,0 +1,33 @@
+import { OrderService } from '@/services/order_service'
+
+export class PatientLabResultService extends OrderService {
+    patientID: number;
+    testTypeID: number;
+
+    constructor(patientId: number) {
+        super()
+        this.patientID = patientId
+        this.testTypeID = -1
+    }
+
+    getTestTypeID() {
+        return this.testTypeID
+    }
+
+    setTestTypeID(test: number) {
+        this.testTypeID = test
+    }
+
+    getTestsWithoutResults() {
+        return OrderService.getOrders(this.patientID, {
+            status: 'drawn'
+        })
+    }
+
+    getTestIndicators() {
+      return OrderService.getJson(
+        `lab/test_result_indicators?`, {
+        'test_type_id': this.testTypeID
+      })
+    }
+}
