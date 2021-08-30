08b4ce287a27048794e7b710db59a2faa5846253
Andrew Mfune <andiemfune@gmail.com>
2021-08-30 14:29:04 +0200

    Able to save lab results

--- src/services/patient_lab_result_service.ts
+++ src/services/patient_lab_result_service.ts
@@ -1,13 +1,29 @@
 import { OrderService } from '@/services/order_service'
+import { AppEncounterService } from './app_encounter_service';
 
-export class PatientLabResultService extends OrderService {
+export class PatientLabResultService extends AppEncounterService {
     patientID: number;
     testTypeID: number;
-
+    resultDate: string;
+    testID: number;
     constructor(patientId: number) {
-        super()
+        super(patientId, 57)
         this.patientID = patientId
         this.testTypeID = -1
+        this.resultDate = ''
+        this.testID = -1
+    }
+
+    createLabResult(measures: Array<any>) {
+        return AppEncounterService.postJson(`lab/tests/${this.testID}/results`, {
+            'encounter_id': this.encounterID,
+            date: this.resultDate,
+            measures
+        })
+    }
+
+    getTestID() {
+        return this.testID
     }
 
     getTestTypeID() {
@@ -17,6 +33,14 @@ export class PatientLabResultService extends OrderService {
     setTestTypeID(test: number) {
         this.testTypeID = test
     }
+
+    setResultDate(date: string) {
+        this.resultDate = date
+    }
+
+    setTestID(test: number) {
+        this.testID = test
+    }   
 
     getTestsWithoutResults() {
         return OrderService.getOrders(this.patientID, {
@@ -30,4 +54,6 @@ export class PatientLabResultService extends OrderService {
         'test_type_id': this.testTypeID
       })
     }
+
+    
 }
