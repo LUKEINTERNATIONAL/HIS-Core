c925a1f09514271bd33f6a709487e17c7e952eb3
Andrew Mfune <andiemfune@gmail.com>
2021-08-31 11:07:06 +0200

    Skip summary page on prescription

--- src/apps/ART/views/encounters/Prescription.vue
+++ src/apps/ART/views/encounters/Prescription.vue
@@ -1,5 +1,5 @@
 <template>
-    <his-standard-form @onIndex="fieldComponent=''" :activeField="fieldComponent" :cancelDestinationPath="cancelDestination" :fields="fields" @onFinish="onSubmit"/>
+    <his-standard-form :skipSummary="true" @onIndex="fieldComponent=''" :activeField="fieldComponent" :cancelDestinationPath="cancelDestination" :fields="fields" @onFinish="onSubmit"/>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
