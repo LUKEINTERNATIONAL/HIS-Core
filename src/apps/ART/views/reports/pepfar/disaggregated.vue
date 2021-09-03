a51ad72164323b292a97e73fbab71fbc430b3d58
Andrew Mfune <andiemfune@gmail.com>
2021-09-03 11:57:30 +0200

    Improve row calculations

--- src/apps/ART/views/reports/pepfar/disaggregated.vue
+++ src/apps/ART/views/reports/pepfar/disaggregated.vue
@@ -13,15 +13,21 @@ import { Field } from "@/components/Forms/FieldInterface"
 import ReportMixin from "@/apps/ART/views/reports/ReportMixin.vue"
 import { DisaggregatedReportService, AGE_GROUPS } from "@/apps/ART/services/reports/pepfar/disaggregated_service"
 import { toastWarning } from '@/utils/Alerts'
-import { isEmpty } from "lodash"
+import { isEmpty, uniq } from "lodash"
 
 export default defineComponent({
     mixins: [ReportMixin],
     data: () => ({
         ageGroupCohort: {} as any,
-        totalFemale: [[], [], [], []] as Array<any>,
-        totalMales: [[], [], [], []] as Array<any>,
-        totalAbsolutePregnant: [] as Array<number>,
+        totalNewF: [] as Array<any>,
+        totalCurF: [] as Array<any>,
+        totalIptF: [] as Array<any>,
+        totalTbF:  [] as Array<any>,
+        totalNewM: [] as Array<any>,
+        totalCurM: [] as Array<any>,
+        totalIptM: [] as Array<any>,
+        totalTbM:  [] as Array<any>,
+        pregnantF: [] as Array<any>
     }),
     async created() {
         this.fields = this.getFields()
@@ -43,13 +49,22 @@ export default defineComponent({
             ]
         },
         async buildTableRows() {
+            const female = await this.buildFemaleRows()
+            const male = await this.buildMaleRows()
+            const pregnant = await this.buildFemalePregnantRows()
+            this.report.setGender('breastfeeding')
+            this.report.setAgeGroup('all')
+            const breastFeeding = await this.buildBreastFeedingRows()
+            const totalMale = this.buildAllMaleRow()
+            const totalNotPregnant = this.buildFemaleNotPregnantRow()
             return [
-                ...(await this.buildFemaleRows()),
-                ...(await this.buildMaleRows()),
-                this.buildAllMaleRow(),
-                ...(await this.buildFemalePregnantRows()),
-                this.buildFemaleNotPregnantRow(),
-                ...(await this.buildBreastFeedingRows())
+                ...female,
+                ...male,
+                totalMale,
+                ...pregnant,
+                totalNotPregnant,
+                ...breastFeeding,
+ 
             ].map((r: any, i: number) => [
                 i+1,
                 r[0],
@@ -75,63 +90,71 @@ export default defineComponent({
             }
             return res
         },
-        setTotalFemale(index: number, data: Array<any>) {
-            this.totalFemale[index] = [...this.totalFemale[index], ...data]
-        },
-        setTotalMale(index: number, data: Array<any>) {
-            this.totalMales[index] = [...this.totalMales[index], ...data]
-        },
-        setTotalPregnant(data: any) {
-            this.totalAbsolutePregnant = [...this.totalAbsolutePregnant, ...data]
-        },
         buildAllMaleRow() {
-            return ['All', 'Male', ...this.totalMales]
+            return [
+                'All', 
+                'Male', 
+                this.totalNewM,
+                this.totalCurM,
+                this.totalIptM,
+                this.totalTbM
+            ]
         },
         buildFemaleNotPregnantRow() {
-            const row = this.totalFemale.map((females: any) => {
-                return females.filter((f: number) => !this.totalAbsolutePregnant.includes(f))
-            })
-            return ['All', 'FNP', ...row]
+            const row = [ 
+                this.totalNewF, 
+                this.totalCurF, 
+                this.totalIptF,
+                this.totalTbF
+            ].map((data: any) => data.filter((i: any) => !this.pregnantF.includes(i)))
+            return [ 'All', 'FNP', ...row ]
         },
         buildFemaleRows() {
+            this.report.setGender('female')
             return this.buildRows('F', AGE_GROUPS, 
             (group: string, txNew: any, txCur: any, txIpt: any, txTb: any) => {
-                this.setTotalFemale(0, txNew)
-                this.setTotalFemale(1, txCur)
-                this.setTotalFemale(2, txIpt)
-                this.setTotalFemale(3, txTb)
-                return [group, 'Female', txNew, txCur, txIpt, txTb]
+                this.totalNewF = uniq(this.totalNewF.concat(txNew))
+                this.totalCurF = uniq(this.totalCurF.concat(txCur))
+                this.totalIptF = uniq(this.totalIptF.concat(txIpt))
+                this.totalTbF  = uniq(this.totalTbF.concat(txTb))
+                return [ group, 'Female', txNew, txCur, txIpt, txTb ]
             })
         },
         buildMaleRows() {
+            this.report.setGender('male')
             return this.buildRows('M', AGE_GROUPS, 
             (group: string, txNew: any, txCur: any, txIpt: any, txTb: any) => {
-                this.setTotalMale(0, txNew)
-                this.setTotalMale(1, txCur)
-                this.setTotalMale(2, txIpt)
-                this.setTotalMale(3, txTb)
-                return [group, 'Male', txNew, txCur, txIpt, txTb]   
+                this.totalNewM = uniq(this.totalNewM.concat(txNew))
+                this.totalCurM = uniq(this.totalCurM.concat(txCur))
+                this.totalIptM = uniq(this.totalIptM.concat(txIpt))
+                this.totalTbM  = uniq(this.totalTbM.concat(txTb))
+                return [ group, 'Male', txNew, txCur, txIpt, txTb ]   
             })
         },
         buildFemalePregnantRows() {
+            this.report.setGender('pregnant')
             return this.buildRows('F', ['Pregnant'], 
                 (_: string, txNew: any, txCur: any, txIpt: any, txTb: any) => {
-                this.setTotalPregnant(txNew)
-                this.setTotalPregnant(txCur)
-                this.setTotalPregnant(txIpt)
-                this.setTotalPregnant(txTb)
+                this.pregnantF = uniq(this.pregnantF.concat(txNew))
+                this.pregnantF = uniq(this.pregnantF.concat(txCur))
+                this.pregnantF = uniq(this.pregnantF.concat(txIpt))
+                this.pregnantF = uniq(this.pregnantF.concat(txTb))
                 return [ 'All', 'FP', txNew, txCur, txIpt, txTb]
             })
         },
         buildBreastFeedingRows() {
+            this.report.setGender('breastfeeding')
             return this.buildRows('F', ['Breastfeeding'], 
             (_: string, txNew: any, txCur: any, txIpt: any, txTb: any) => {
+                this.pregnantF = uniq(this.pregnantF.concat(txNew))
+                this.pregnantF = uniq(this.pregnantF.concat(txCur))
+                this.pregnantF = uniq(this.pregnantF.concat(txIpt))
+                this.pregnantF = uniq(this.pregnantF.concat(txTb))
                 return ['All', 'FBf', txNew, txCur, txIpt, txTb]
             })
         },
         async buildRows(category: string, ageGroups: Array<string>, onFormat: Function) {
             const rows = []
-            this.report.setGender(category === 'M' ? 'Male' : 'Female')
             for(const i in ageGroups) {
                 let txNew = []
                 let txCurr= []
