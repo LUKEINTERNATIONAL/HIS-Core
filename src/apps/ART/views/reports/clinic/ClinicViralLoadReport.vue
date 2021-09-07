119e6cb00fef1b66d398fc779a605c0f60f18400
Andrew Mfune <andiemfune@gmail.com>
2021-09-07 13:35:04 +0200

    Initial Implementation of Viral load report

---
+++ src/apps/ART/views/reports/clinic/ClinicViralLoadReport.vue
@@ -0,0 +1,60 @@
+<template>
+    <clinic-report-template
+        :title="title"
+        :period="period"
+        :totalClients="totalClients"
+        > 
+        <report-table :rows="rows" :columns="columns"> </report-table>
+    </clinic-report-template>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+import { PatientReportService } from "@/apps/ART/services/reports/patient_report_service"
+import ReportMixin from "@/apps/ART/views/reports/ReportMixin.vue"
+
+export default defineComponent({
+    mixins: [ReportMixin],
+    data: () => ({
+        title: 'Other outcome report',
+        totalClients: [],
+        rows: [] as Array<any>,
+        columns: [
+            'ARV#', 'Gender', 'Birthdate', 'Specimen', 'Ordered', 'Result', 'Released'
+        ]
+    }),
+    watch: {
+        isReady: {
+            async handler(y: boolean) {
+                const { query } = this.$route
+                if (y && query.result_type) {
+                    await this.init(this.startDate, this.endDate, query.result_type)
+                }
+            },
+            immediate: true,
+            deep: true
+        }
+    },
+    methods: {
+        async init(startDate: string, endDate: string, resultType: any) {
+            this.report = new PatientReportService()
+            this.report.setStartDate(startDate)
+            this.report.setEndDate(endDate)
+            this.setRows((await this.report.getViralLoadResults(resultType.toLowerCase())))
+        },
+        async setRows(data: Array<any>) {
+            data.forEach((d: any) => {
+               this.rows.push([
+                    d.arv_number,
+                    d.gender,
+                    this.toDate(d.birthdate),
+                    d.specimen,
+                    this.toDate(d.order_date),
+                    d.result,
+                    this.toDate(d.result)                    
+               ])
+            })
+        }
+    }
+})
+</script>
