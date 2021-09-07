c6f6428a50b0b17d7d36a9a1fe58e381691be25e
Andrew Mfune <andiemfune@gmail.com>
2021-09-07 13:34:54 +0200

    Initial Implementation of Regimen formulation report

---
+++ src/apps/ART/views/reports/clinic/ClinicRegimenFormulationReport.vue
@@ -0,0 +1,56 @@
+<template>
+    <clinic-report-template
+        :title="title"
+        :period="period"
+        :totalClients="totalClients"
+        > 
+        <report-table :rows="rows" :columns="columns"> </report-table>
+    </clinic-report-template>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+import { PatientReportService } from "@/apps/ART/services/reports/patient_report_service"
+import ReportMixin from "@/apps/ART/views/reports/ReportMixin.vue"
+
+export default defineComponent({
+    mixins: [ReportMixin],
+    data: () => ({
+        title: 'Other outcome report',
+        totalClients: [],
+        rows: [] as Array<any>,
+        columns: [
+            'ARV#', 'Gender', 'DOB'
+        ]
+    }),
+    watch: {
+        isReady: {
+            async handler(y: boolean) {
+                const { query } = this.$route
+                if (y && query.regimen && query.formulation) {
+                    await this.init(this.startDate, this.endDate, query.regimen, query.formulation)
+                }
+            },
+            immediate: true,
+            deep: true
+        }
+    },
+    methods: {
+        async init(startDate: string, endDate: string, regimen: any, formulation: any) {
+            this.report = new PatientReportService()
+            this.report.setStartDate(startDate)
+            this.report.setEndDate(endDate)
+            this.setRows((await this.report.getOtherOutcome(regimen, formulation)))
+        },
+        async setRows(data: Array<any>) {
+            data.forEach((d: any) => {
+               this.rows.push([
+                    d.arv_number,
+                    d.gender,
+                    this.toDate(d.birthdate)
+               ])
+            })
+        }
+    }
+})
+</script>
