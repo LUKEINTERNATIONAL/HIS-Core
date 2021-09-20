cef7c45a3d9357d0724a9281e0f08e2ff6c63760
Andrew Mfune <andiemfune@gmail.com>
2021-09-20 16:26:21 +0200

    Using custom function to generate csv.
    Fixed download script

--- .d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'convert-array-to-csv';
--- src/utils/Export.ts
+++ src/utils/Export.ts
@@ -1,13 +1,18 @@
 import jsPDF from "jspdf"
 import autoTable from 'jspdf-autotable'
-import csvConverter from "convert-array-to-csv"
+
+function convertToCsv(list: Array<any>) {
+  let str = ''
+  list.forEach((l: Array<string>) => str += l.join(',') + '\n')
+  return str.toString()
+}
 
 export function toCsv(header: Array<any>, rows: Array<any>, fileName='document') {
-    const csvContent = "data:text/csv;charset=utf-8," + csvConverter(rows, { header, seperator: ';'})
-    const encodedUri = encodeURI(csvContent);
+    const csvContent = convertToCsv([header].concat(rows))
+    const csvData = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
     const link = document.createElement("a");
     link.setAttribute('id', 'csv')
-    link.setAttribute("href", encodedUri);
+    link.href = window.URL.createObjectURL(csvData);
     link.setAttribute("download", `${fileName}.csv`);
     document.body.appendChild(link);
     link.click();
