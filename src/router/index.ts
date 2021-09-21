a5c0be97f556b8e440f561a3be4f2c8790bec714
Andrew Mfune <andiemfune@gmail.com>
2021-09-21 14:20:51 +0200

    Can now print location

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,7 @@ import Configuration from '@/views/Configuration.vue'
 import Settings from '@/components/ART/Settings.vue'
 import HisApps from '@/apps/his_apps';
 import SessionDate from "@/views/SessionDate.vue"
+import PrintLocation from "@/views/PrintLocation.vue"
 import HostConfig from '@/views/HostConfig.vue'
 import ProgramManagement from "@/views/ProgramManagement.vue"
 import LabResults from "@/views/LabResults.vue"
@@ -62,6 +63,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/session/date',
     name: 'Session Date',
     component: SessionDate
+  },
+  {
+    path: '/print/location',
+    name: 'Print Location',
+    component: PrintLocation
   },
   {
     path: '/select_hc_location',
