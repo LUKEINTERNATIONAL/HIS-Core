7207ae28d00e109bc0c2bda3c0110c3fcf4ec1fc
Andrew Mfune <andiemfune@gmail.com>
2021-10-01 12:18:11 +0200

    Added filing number prefix loader

---
+++ src/apps/ART/services/filing_number_service.ts
@@ -0,0 +1,62 @@
+import { Service } from "@/services/service";
+import { PrintoutService } from "@/services/printout_service";
+import { GlobalPropertyService } from "@/services/global_property_service";
+
+export class FilingNumberService extends Service {
+    patientID: number;
+    activePrefix: string;
+    dormantPrefix: string;
+    constructor() {
+        super()
+        this.patientID = 0
+        this.activePrefix = ''
+        this.dormantPrefix = ''
+    }
+
+    setPatientID(patientID: number) {
+        this.patientID = patientID
+    }
+
+    async loadFilingPrefix() {
+        const req = await GlobalPropertyService.get('filing.number.prefix')
+        if (req) {
+            const [ activePrefix, dormantPrefix] = req['filing.number.prefix'].split(',')
+            this.activePrefix = activePrefix
+            this.dormantPrefix = dormantPrefix
+        }
+    }
+
+    archiveFilingNumber() {
+        return Service.getJson(`archive_active_filing_number`, {
+            'patient_id': this.patientID
+        })
+    }
+
+    archivePatient(secondaryPatient: number, identifier: string | number) {
+        return Service.postJson('swap_active_number', {
+            'primary_patient_id': this.patientID,
+            'secondary_patient_id': secondaryPatient,
+            identifier
+        })
+    }
+
+
+    getArchivingCandidates(page=0, pageSize=10) {
+        return Service.getJson(`archiving_candidates`, {
+            page,'page_size': pageSize
+        })
+    }
+
+    getArchived() {
+        return Service.getJson(``)
+    }
+
+    getPastFilingNumbers() {
+        return Service.getJson(`patients/${this.patientID}/past_filing_numbers`)
+    }
+
+    printFilingNumber(filingNumber: number) {
+        const printer = new PrintoutService()
+        return printer.printLbl(`patients/${filingNumber}/labels/filing_number`) 
+    }
+}
