dee550334a5b1fa2e6067443be517198c9b38a38
Andrew Mfune <andiemfune@gmail.com>
2021-10-04 13:27:30 +0200

    Can now conditionary display task items on the patient dashboard

--- src/apps/interfaces/TaskInterface.ts
+++ src/apps/interfaces/TaskInterface.ts
@@ -2,6 +2,7 @@ export interface TaskInterface {
     id: string;
     name: string;
     description?: string;
+    condition?: (globalPropertyService: any) => boolean;
     url?: string;
     action?: Function;
     icon: string;
