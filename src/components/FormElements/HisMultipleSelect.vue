8bbf25087e3e9ebb32cc006b6ad37f4a7df0e7c7
Andrew Mfune <andiemfune@gmail.com>
2021-10-06 14:33:34 +0200

    Using primary colors to highlight selections

--- src/components/FormElements/HisMultipleSelect.vue
+++ src/components/FormElements/HisMultipleSelect.vue
@@ -6,7 +6,7 @@
           <ion-chip color="danger" @click="uncheck(item.label)">{{item.label}}</ion-chip>
         </span>
         <ion-list class='view-port-content'>
-          <ion-item v-for="(entry, index) in filtered" :key="index" :color="entry.isChecked ? 'light':''">
+          <ion-item v-for="(entry, index) in filtered" :key="index" :color="entry.isChecked ? 'primary':''">
             <ion-label> 
               <ion-text>
                 {{ entry.label }} 
