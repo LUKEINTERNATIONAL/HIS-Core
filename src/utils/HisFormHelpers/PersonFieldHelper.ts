c16f48420abde1747ade9ed706c295882e6029de
Andrew Mfune <andiemfune@gmail.com>
2021-10-08 16:28:36 +0200

    Hide option to select foreigner as current region

--- src/utils/HisFormHelpers/PersonFieldHelper.ts
+++ src/utils/HisFormHelpers/PersonFieldHelper.ts
@@ -167,7 +167,10 @@ export default {
             type: FieldType.TT_SELECT,
             computedValue: (val: Option) => ({person: val.label}),
             validation: (val: any) => Validation.required(val),
-            options: () => getRegions()
+            options: async () => {
+                const regions = await getRegions()
+                return regions.filter((d: any) => !d.label.match(/Foreign/i))
+            } 
         }
     },
     getCurrentDistrictField() {
