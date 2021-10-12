0d656c25c75643d14fb6080bceb791f4f0dbac80
Andrew Mfune <andiemfune@gmail.com>
2021-10-12 21:44:55 +0200

    Updated Application interface.. These changes are breaking

--- src/apps/interfaces/AppInterface.ts
+++ src/apps/interfaces/AppInterface.ts
@@ -1,20 +1,5 @@
-import { Option } from "@/components/Forms/FieldInterface";
-import { TaskInterface } from "./TaskInterface";
 import { RouteRecordRaw } from 'vue-router';
-
-interface ProgramCardInfoInterface {
-    (programInfo: any): Array<Option>;
-}
-
-interface AlertsInterface {
-    (id: number): Promise<Option[]>;
-}
-
-interface PatientDashboarInterface {
-    readonly alerts: AlertsInterface;
-    readonly programCardInfo: ProgramCardInfoInterface;
-    readonly tasks: Record<string, TaskInterface[]>;
-}
+import { TaskInterface } from './TaskInterface';
 
 export interface ReportGroupInterface {
     name: string;
@@ -28,27 +13,80 @@ export interface ReportInterface {
     pathUrl?: string;    
 }
 
-export interface ActivityInterface {
-    readonly value: string;
-    selected: boolean;
-}
-
 export interface PreferenceInterface {
     name: string;
-    component?: string;
-    value?: string;
-    route?: string;
+    route: string;
 }
+
+export interface IdentifierInterface {
+    id: number;
+    name: string;
+    useForSearch: boolean;
+}
+
+export interface GeneralDataInterface {
+    label: string;
+    value: string;
+}
+
 export interface AppInterface {
+    /**
+     * Map primary key value of a program here. All records will
+     * associate with this program ID
+    */
     readonly programID: number;
+    /**
+     * Name of the program as it will be presented on the Application Selection
+     * list
+     */
     readonly applicationName: string;
+    /**
+     * URL of the applicationIcon 
+     */
     readonly applicationIcon: string;
+    /**
+     * Description of the app as it will appear during Application Selection
+     */
     readonly applicationDescription: string;
+    /**
+     * Routes to different application resources
+     */
     readonly appRoutes: Array<RouteRecordRaw>;
-    readonly appOverviewComponent: any;
-    readonly patientDashboard: PatientDashboarInterface;
-    readonly reports: Array<ReportGroupInterface>;
-    activities: Array<ActivityInterface>;
-    preferences: { [key: string]: Array<PreferenceInterface>};
-    preferenceComponents?: any;
+    /**
+     * Identifiers available for search / reference that are unique to this programme
+    */
+    readonly patientIdentifiers?: IdentifierInterface;
+    /**
+     * Component that is rendered on the landing page that'll typically
+     * show some sort of summary of activities
+     */
+    readonly homeOverviewComponent: any; //Vue component
+    /**
+     * Component that is rendered on the patient dashboard
+     */
+    readonly patientDashboardComponent: any; // Vue component
+    /**
+     * Define all reports available for program
+    */
+    readonly reports?: ReportGroupInterface[];
+    /**
+     * Define list of all application configurations
+     */
+    readonly preferences?: PreferenceInterface[];
+    /**
+     * primary patient activities
+     */
+    readonly primaryPatientActivites: TaskInterface[];
+    /**
+     * secondary patient activities
+     */
+    readonly secondaryPatientActivites: TaskInterface[];
+    /**
+     * Onload event when the program is selected
+    */
+    readonly onLoad?: () => void;
+    /**
+     * Overrall summary/state of the patient on this program
+     */
+    readonly getPatientProgramSummary?: (patientID: number) => Record<string, GeneralDataInterface[]>;
 }
