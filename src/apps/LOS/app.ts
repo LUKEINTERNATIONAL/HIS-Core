5d6e372724c341ae309ef4dc58de5ed254ac35d2
Andrew Mfune <andiemfune@gmail.com>
2021-10-14 07:43:14 +0200

    Initial Lab ordering system configuration

---
+++ src/apps/LOS/app.ts
@@ -0,0 +1,15 @@
+import { AppInterface } from "../interfaces/AppInterface";
+
+const LOS: AppInterface = {
+    programID: 23,
+    applicationName: 'LOS',
+    applicationIcon: 'order.png',
+    applicationDescription: "Lab Order System",
+    appRoutes: [],
+    primaryPatientActivites: [],
+    secondaryPatientActivites: [],
+    globalPropertySettings: [],
+    homeOverviewComponent: {},
+    confirmationSummary: () => ({}),
+}
+export default LOS
