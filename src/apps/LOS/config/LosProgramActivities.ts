cd12dba8f7c5717fa985f88bdcc61d017c384fe3
Andrew Mfune <andiemfune@gmail.com>
2021-10-19 13:15:27 +0200

    (LOS)Removed programme activity for order tests

--- src/apps/LOS/config/LosProgramActivities.ts
+++ src/apps/LOS/config/LosProgramActivities.ts
@@ -2,17 +2,9 @@ import { TaskInterface } from "../../interfaces/TaskInterface"
 
 export const PRIMARY_ACTIVITIES: TaskInterface[] = [
   {
-    id: "order tests",
-    name: "Order Tests",
-    icon: "social_history.png",
-    action: ({patient}: any, router: any) => {
-      router.push(`/los/forms/order/${patient.patient_id}?type=ORDER_TESTS`)
-    }
-  },
-  {
     id: "draw samples",
     name: "Draw Samples",
-    icon: "hts.png",
+    icon: "social_history.png",
     action: ({patient}: any, router: any) => {
       router.push(`/los/forms/order/${patient.patient_id}?type=DRAW_SAMPLES`)
     }
