89083d7c0ded24ec6087fc69a443549da998286c
Andrew Mfune <andiemfune@gmail.com>
2021-10-21 11:01:27 +0200

    Capitalise first letter of keyboard entries

--- src/components/Keyboard/KbHandler.ts
+++ src/components/Keyboard/KbHandler.ts
@@ -1,6 +1,5 @@
 export default function kbHandler(newInput: string, accumulator: string): string {
     let output = accumulator
-
     if (newInput.match(/delete|del/i)) {
         output = output.substring(0, output.length - 1)
     } else if (newInput.match(/space/i)) {
@@ -10,6 +9,8 @@ export default function kbHandler(newInput: string, accumulator: string): string
     } else {
         output = `${accumulator}${newInput}`
     }
+    if (typeof output === 'string' && output) {
+        output = output.charAt(0).toUpperCase() + output.slice(1)
+    }
     return output
-
 }
