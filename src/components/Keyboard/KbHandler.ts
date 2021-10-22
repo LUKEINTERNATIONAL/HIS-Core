e802645ae6478f110ee4253521365f06a4f40280
Andrew Mfune <andiemfune@gmail.com>
2021-10-22 10:31:17 +0200

    Deleting text input overrides unknown/n/a values

--- src/components/Keyboard/KbHandler.ts
+++ src/components/Keyboard/KbHandler.ts
@@ -1,7 +1,12 @@
 export default function kbHandler(newInput: string, accumulator: string): string {
     let output = accumulator
     if (newInput.match(/delete|del/i)) {
-        output = output.substring(0, output.length - 1)
+        // Override Unknown text with new input
+        if (output.match(/unknown/i) || output.match(/n\/a/i)) {
+            output = ''
+        } else {
+            output = output.substring(0, output.length - 1)
+        }
     } else if (newInput.match(/space/i)) {
         output = `${accumulator} `
     } else if (newInput.match(/unknown/i)) {
