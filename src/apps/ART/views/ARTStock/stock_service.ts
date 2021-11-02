1464086cf3248a5689a0e45077efef6825842350
Martin Suleman <sulemartin87@gmail.com>
2021-11-02 11:33:15 +0200

    cleaned code for stock service

--- src/apps/ART/views/ARTStock/stock_service.ts
+++ src/apps/ART/views/ARTStock/stock_service.ts
@@ -214,7 +214,7 @@ export class StockService extends Service {
 		return Service.postJson('/pharmacy/batches', items);
 	}
 	getItems() {
-		return Service.getJson('pharmacy/items', {paginate: false})
+		return Service.getJson('pharmacy/items', { paginate: false })
 	}
 	relocateItems(batchID: number, items: any) {
 		return Service.postJson(`pharmacy/items/${batchID}/reallocate`, items)
@@ -225,121 +225,50 @@ export class StockService extends Service {
 	drugList() {
 		return finalList;
 	}
-	static getShortName(drugID: number){
+	static getShortName(drugID: number) {
 		return finalList.filter(d => d.drugID === drugID)[0].shortName
 	}
-	static getPackSize(drugID: number){
+	static getPackSize(drugID: number) {
 		return finalList.filter(d => d.drugID === drugID)[0].packSizes[0]
 	}
+	// Waiting to get new list of paediatric drugs and adult drugs
+	//     var adults_drugs_hash = {
+	// 	"976": "Darunavir 600mg",
+	// 	"977": "Ritonavir 100mg",
+	// 	"978": "Etravirine 100mg",
+	// 	"954": "Raltegravir 400mg",
+	// 	"22": "NVP (Nevirapine 200 mg tablet)",
+	// 	"969": "ABC/3TC (Abacavir and Lamivudine 600/300mg tablet)",
+	// 	"731": "AZT/3TC/NVP (300/150/200mg tablet)",
+	// 	"39": "AZT/3TC (Zidovudine and Lamivudine 300/150mg)",
+	// 	"11": "EFV (Efavirenz 600mg tablet)",
+	// 	"735": "TDF/3TC/EFV (300/300/600mg tablet)",
+	// 	"734": "TDF/3TC (Tenofavir and Lamivudine 300/300mg tablet",
+	// 	"932": "ATV/r (Atazanavir 300mg/Ritonavir 100mg)",
+	// 	"73": "LPV/r (Lopinavir and Ritonavir 200/50mg tablet)",
+	// 	"576": "Cotrimoxazole (960mg)",
+	// 	"297": "Cotrimoxazole (480mg tablet)",
+	// 	"931": "INH or H (Isoniazid 300mg tablet)",
+	// 	"954": "Raltegravir 400mg",
+	// 	"1056": "Rifapentine 150mg",
+	// 	"976": "Darunavir 600mg",
+	// 	"982": "Dolutegravir (50mg tablet)",
+	// 	"983":  "TDF300/3TC300/DTG50",
+	// 	"1039":  "Pyridoxine (25mg)",
+	// 	"977":  "Ritonavir 100mg",
+	//     };
+
+	//     var paedsDrugsHash = {
+	// 	"733": "Abacavir / Lamivudine 60 / 30mg, tin of 60 tablets",
+	// 	"968": "Nevirapine 50mg, tin of 60 tablets",
+	// 	"732": "Zidovudine / lamivudine /Nevirapine 30 / 60 / 50mg, tin of 60 tablets",
+	// 	"736": "Zidovudine / lamivudine 60 / 30mg, tin of 60 tablets",
+	// 	"30": "Efavirenz 200mg, tin of 90 tablets",
+	// 	"74": "Lopinavir / Ritonavir 100 / 25mg, tin of 60 tablets",
+	// 	"979": "LPV/r pellets",
+	// 	"963": "Cotrimoxazole 120mg, blister pack of 1000 dispersible tablets",
+	// 	"24": "Isoniazid 100mg, tin of 100 tablets",
+	// 	"1044": "ABC/3TC (Abacavir and Lamivudine 120/60mg tablet)",
+	// 	"1043": "Raltegravir 25mg",
+	//     };
 }
-
-// var drugs: any = {};
-// finalList.forEach(element => {
-// 	drugs[element.drugID] = {
-// 		shortName: element.shortName,
-// 		fullName: element.fullName
-// 	}
-// });
-// var drug_short_full_name = drugs;
-// var drugCMSNames: any = {};
-// finalList.forEach(element => {
-// 	drugCMSNames[element.shortName] = element.fullName;
-// });
-
-// var drug_short_names: any = {};
-// finalList.forEach(element => {
-// 	drug_short_names[element.fullName] = element.shortName;
-// });
-
-// var drugs_unformatted = Object.keys(drug_short_names);
-// var formatted_drugs = drugs_unformatted;
-// var drug_map_hash: any = {};
-// finalList.forEach(element => {
-// 	drug_map_hash[element.drugID] = element.fullName;
-// });
-
-// export function getDrugPackSizes() {
-// 	var drug_cms_packsizes: any = {};
-// 	finalList.forEach(element => {
-// 		drug_cms_packsizes[element.shortName] = element.packSizes[0];
-// 	});
-// 	return drug_cms_packsizes;
-// }
-// export function getPackSize(drug_id: any) {
-// 	return finalList.filter((drug: any) => drug.drug_id === drug_id)[0].packSizes[0];
-// }
-//     var drug_weights = {
-// 	"16": ["Lamivudine / Tenofovir disoproxil fumarate 300 / 300mg, tin of 30 tablets", "TDF/3TC (Tenofavir and Lamivudine 300/300mg tablet", "TDF / 3TC ", "30 tabs", 30, 0, "300 / 300mg"],
-// 	"5": ["Zidovudine / lamivudine /Nevirapine 30 / 60 / 50mg, tin of 60 tablets", "AZT/3TC/NVP (60/30/50mg tablet)", "AZT / 3TC / NVP", "60 tabs", 60, 0, "60 / 30 /50mg"],
-// 	"11": ["Lopinavir / Ritonavir 100 / 25mg, tin of 60 tablets", "LPV/r (Lopinavir and Ritonavir 100/25mg tablet)", "LPV / r ", "60 tabs", 60, 0, "100 / 25mg"],
-// 	"17": ["Tenofovir disoproxil fumarate / Lamivudine / Efavirenz 300 / 300 / 600mg, tin of 30 tablets", "TDF/3TC/EFV (300/300/600mg tablet)", "TDF / 3TC / EFV ", "30 tabs", 30, 0, "300 / 300 / 600mg"],
-// 	"6": ["Zidovudine / lamivudine /Nevirapine 300 / 150 / 200mg, tin of 60 tablets", "AZT/3TC/NVP (300/150/200mg tablet)", "AZT / 3TC / NVP", "60 tabs", 60, 0, "300 / 150 / 200mg"],
-// 	"23": ["Cotrimoxazole 120mg, blister pack of 1000 dispersible tablets", "TMP/SMX (Cotrimoxazole 120mg tablet)", "Cotrimoxazole", "1000 tabs", 1000, 0, "100 / 20mg"],
-// 	"1": ["Abacavir / Lamivudine 60 / 30mg, tin of 60 tablets", "ABC/3TC (Abacavir and Lamivudine 60/30mg tablet)", "ABC / 3TC", "60 tabs", 60, 0, "60 / 30mg"],
-// 	"12": ["Lopinavir / Ritonavir 200 / 50mg, tin of 120 tablets", "LPV/r (Lopinavir and Ritonavir 200/50mg tablet)", "LPV / r ", "120 tabs", 120, 0, "200 / 50mg"],
-// 	"7": ["Stavudine / lamivudine 30 / 150mg, tin of 60 tablets", "d4T/3TC (Stavudine Lamivudine 30/150 tablet)", "d4T/3TC", "60 tabs", 60, 0, "30 / 150mg"],
-// 	"18": ["Nevirapine 10mg/ml, 25ml", "NVP (Nevirapine syrup 1.5mL/dose in 25mL bottle)", "NVP", "25ml", 25, 0, "10mg/ml"],
-// 	"2": ["Abacavir sulfate 600mg / Lamivudine 300mg, tin of 30 tablets", "ABC/3TC (Abacavir and Lamivudine 600/300mg tablet)", "ABC / 3TC", "30 tabs", 30, 0, "600 / 300mg"],
-// 	"13": ["Atazanavir / Ritonavir 300 / 100mg, tin of 30 tablets", "ATV/r (Atazanavir 300mg/Ritonavir 100mg)", "ATV / r ", "30 tabs", 30, 0, "300 / 100mg"],
-// 	"24": ["Cotrimoxazole 480mg, tin of 1000 tablets", "Cotrimoxazole (480mg tablet)", "Cotrimoxazole", "1000 tabs", 1000, 0, "400 / 80mg"],
-// 	"19": ["Nevirapine 10mg/ml, 100ml", "NVP (Nevirapine syrup 1mL/dose in 100mL bottle)", "NVP", "100ml", 100, 0, "10mg/ml"],
-// 	"8": ["Lamivudine / Stavudine / Nevirapine 150 / 30 / 200mg, tin of 60 tablets", "d4T/3TC/NVP (30/150/200mg tablet)", "d4T/3TC/NVP", "60 tabs", 60, 0, "30 / 150 / 200mg"],
-// 	"14": ["Nevirapine 50mg, tin of 60 tablets", "NVP (Nevirapine 50 mg tablet)", "NVP", "60 tabs", 60, 0, "50mg"],
-// 	"3": ["Zidovudine / lamivudine 60 / 30mg, tin of 60 tablets", "AZT/3TC (Zidovudine and Lamivudine 60/30 tablet)", "AZT / 3TC", "60 tabs", 60, 0, "60 / 30mg"],
-// 	"25": ["Cotrimoxazole 960mg, blister pack of 1000 tablets", "Cotrimoxazole (960mg)", "Cotrimoxazole", "1000 tabs", 1000, 0, "960mg"],
-// 	"9": ["Efavirenz 200mg, tin of 90 tablets", "EFV (Efavirenz 200mg tablet)", "EFV", "90 tabs", 90, 0, "200mg"],
-// 	"20": ["Isoniazid 100mg, tin of 100 tablets", "INH or H (Isoniazid 100mg tablet)", "Isoniazid (H) ", "100 tabs", 100, 0, "100mg"],
-// 	"26": ["Pyridoxine 50mg tab, 1000 tablets", "Pyridoxine (50mg)", "Pyridoxine", "1000 tabs", 1000, 0, "50mg"],
-// 	"4": ["Zidovudine / lamivudine 300 / 150mg, tin of 60 tablets", "AZT/3TC (Zidovudine and Lamivudine 300/150mg)", "AZT / 3TC", "60 tabs", 60, 0, "300 / 150mg"],
-// 	"15": ["Nevirapine 200mg, tin of 60 tablets", "NVP (Nevirapine 200 mg tablet)", "NVP", "60 tabs", 60, 0, "200mg"],
-// 	"21": ["Isoniazid 300mg, tin of 1000 tablets", "INH or H (Isoniazid 300mg tablet)", "Isoniazid (H) ", "1000 tabs", 1000, 0, "300mg"],
-// 	// "26": ["ABC / 3TC 120 / 60mg 60 tabs", "ABC/3TC (Abacavir and Lamivudine 120/60mg tablet)", "ABC", "60 tabs", 60, 0, "600mg"],
-// 	"27": ["Raltegravir 25mg, 60 tablets", "Raltegravir 25mg", "RAL", "60 tabs", 60, 0, "25mg"],
-// 	"28": ["Raltegravir 400mg, 60 tablets", "Raltegravir 400mg", "RAL", "60 tabs", 60, 0, "400mg"],
-// 	"29": ["Darunavir 600mg, 60 tablets", "Darunavir 600mg", "DRV", "60 tabs", 60, 0, "600mg"],
-// 	"30": ["Dolutegravir 50mg 30 tablets", "Dolutegravir (50mg tablet)", "DTG", "30 tabs", 30, 0, "50mg"],
-// 	"31": ["TDF300/3TC300/DTG50", "TDF300/3TC300/DTG50", "TDF300/3TC300/DTG50", "30 tabs", 30, 0, "300/300/50mg"],
-// 	"32": ["Pyridoxine (25mg)", "Pyridoxine (25mg)", "Pyridoxine (25mg)", "1000 tabs", 1000, 0, "25mg"],
-// 	"33": ["Ritonavir 100mg", "Ritonavir 100mg", "Ritonavir 100mg", "30 tabs", 30, 0, "100mg"],
-//     };
-
-
-//     var adults_drugs_hash = {
-// 	"976": "Darunavir 600mg",
-// 	"977": "Ritonavir 100mg",
-// 	"978": "Etravirine 100mg",
-// 	"954": "Raltegravir 400mg",
-// 	"22": "NVP (Nevirapine 200 mg tablet)",
-// 	"969": "ABC/3TC (Abacavir and Lamivudine 600/300mg tablet)",
-// 	"731": "AZT/3TC/NVP (300/150/200mg tablet)",
-// 	"39": "AZT/3TC (Zidovudine and Lamivudine 300/150mg)",
-// 	"11": "EFV (Efavirenz 600mg tablet)",
-// 	"735": "TDF/3TC/EFV (300/300/600mg tablet)",
-// 	"734": "TDF/3TC (Tenofavir and Lamivudine 300/300mg tablet",
-// 	"932": "ATV/r (Atazanavir 300mg/Ritonavir 100mg)",
-// 	"73": "LPV/r (Lopinavir and Ritonavir 200/50mg tablet)",
-// 	"576": "Cotrimoxazole (960mg)",
-// 	"297": "Cotrimoxazole (480mg tablet)",
-// 	"931": "INH or H (Isoniazid 300mg tablet)",
-// 	"954": "Raltegravir 400mg",
-// 	"1056": "Rifapentine 150mg",
-// 	"976": "Darunavir 600mg",
-// 	"982": "Dolutegravir (50mg tablet)",
-// 	"983":  "TDF300/3TC300/DTG50",
-// 	"1039":  "Pyridoxine (25mg)",
-// 	"977":  "Ritonavir 100mg",
-//     };
-
-//     var paedsDrugsHash = {
-// 	"733": "Abacavir / Lamivudine 60 / 30mg, tin of 60 tablets",
-// 	"968": "Nevirapine 50mg, tin of 60 tablets",
-// 	"732": "Zidovudine / lamivudine /Nevirapine 30 / 60 / 50mg, tin of 60 tablets",
-// 	"736": "Zidovudine / lamivudine 60 / 30mg, tin of 60 tablets",
-// 	"30": "Efavirenz 200mg, tin of 90 tablets",
-// 	"74": "Lopinavir / Ritonavir 100 / 25mg, tin of 60 tablets",
-// 	"979": "LPV/r pellets",
-// 	"963": "Cotrimoxazole 120mg, blister pack of 1000 dispersible tablets",
-// 	"24": "Isoniazid 100mg, tin of 100 tablets",
-// 	"1044": "ABC/3TC (Abacavir and Lamivudine 120/60mg tablet)",
-// 	"1043": "Raltegravir 25mg",
-//     };
