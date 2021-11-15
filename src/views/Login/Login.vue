d762ec5b8dbccaf290bcade09e90393b65dcfe5b
Andrew Mfune <andiemfune@gmail.com>
2021-11-15 19:32:29 +0200

    Using Ionic  components to style LOgin

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,60 +1,65 @@
 <template>
   <ion-page>
-    <ion-content :fullscreen="true">
-      <div id="login" class="container">
-        <div class="header">
-          <div id="app-name">
-            <b style="color: #8b4513">National</b>&nbsp;<b
-              style="color: #cd853f"
-              >EMR</b
-            >
-          </div>
-          <span>{{ versionDetails.title }}</span
-          >| &nbsp;Series: <span>{{ versionDetails.series }}</span
-          >| &nbsp;Version: <span>{{ versionDetails.version }}</span>
-          &nbsp;
-        </div>
-        <Inputs v-bind:keys="keys"></Inputs>
-      </div>
+    <ion-header> 
+      <ion-toolbar>
+        <ion-title> 
+          <ion-label :style="{fontWeight: 'bolder', fontSize: '1.9em'}"> 
+            <b style="color: #8b4513">National </b>
+            <b style="color: #cd853f">EMR</b>
+          </ion-label>
+        </ion-title>
+        <ion-label slot="end">
+          Version: <b>{{ versionDetails.version }}</b>
+        </ion-label>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <inputs v-bind:keys="keys"></inputs>
     </ion-content>
     <ion-footer>
-      <span class="" id="coat">
-        <img
-          class="logos"
-          id="mw-img"
-          src="/assets/images/login-logos/Malawi-Coat_of_arms_of_arms.png"
-          alt="Malawi Coat of Arms logo"
-        />
-      </span>
-      <span class="" id="pepfar">
-        <img
-          id="donor-img"
-          class="logos"
-          src="/assets/images/login-logos/PEPFAR.png"
-          alt="PEPFAR logo"
-        />
-      </span>
-      <ion-button
-        class="config"
-        router-link="/settings/host"
-        v-if="showConfig()"
-      >
-        Configuration
-      </ion-button>
+      <ion-toolbar>
+        <span>
+          <img
+            id="coat"
+            src="/assets/images/login-logos/Malawi-Coat_of_arms_of_arms.png"
+            alt="Malawi Coat of Arms logo"
+          />
+          <img
+            id="pepfar"
+            src="/assets/images/login-logos/PEPFAR.png"
+            alt="PEPFAR logo"
+          />
+        </span>
+        <ion-button
+          class="config"
+          router-link="/settings/host"
+          v-if="showConfig()">
+          Configuration
+        </ion-button>
+      </ion-toolbar>
     </ion-footer>
   </ion-page>
 </template>
 
 <script lang="ts">
 import Inputs from "./LoginCustomPage.vue";
-import { IonButton, IonPage, IonContent, IonFooter } from "@ionic/vue";
 import {LOGIN_KEYBOARD} from "@/components/Keyboard/KbLayouts";
+import { 
+  IonButton, 
+  IonPage, 
+  IonContent, 
+  IonFooter,
+  IonHeader,
+  IonToolbar
+} from "@ionic/vue";
 export default {
   name: "login",
   components: {
     Inputs,
     IonButton,
     IonPage,
+    IonHeader,
+    IonToolbar,
     IonContent,
     IonFooter,
   },
@@ -62,9 +67,7 @@ export default {
     return {
       versionDetails: {
         type: String,
-        title: "HIS-EMR",
         version: "1.0.0",
-        series: "4",
       },
       keys: LOGIN_KEYBOARD,
     };
@@ -78,44 +81,18 @@ export default {
 };
 </script>
 
-
 <style scoped>
-#container {
-  margin: 15px;
-  /*border-style: solid;
-  border-width: 1px;*/
-  height: 95%;
+#coat {
+  width: 8vw;
+  height: 8vw;
 }
-
-.logos {
-  bottom: 5px;
-  width: 90px;
-  height: 90px;
-  margin-left: 10px;
-  /* float: left; */
-  /* position: absolute; */
+#pepfar {
+  width: 6vw;
+  height: 6vw;
 }
 .config {
   margin: auto;
   float: right;
   margin-top: 20px;
-}
-.header span {
-  font-weight: bolder;
-}
-
-.header {
-  border-style: solid;
-  height: 40px;
-  text-align: right;
-  margin: 10px;
-  padding-right: 10px;
-  border-width: 0px 0px 1px 0px;
-  border-color: rgb(204, 204, 204);
-}
-
-#app-name {
-  float: left;
-  font-size: 2.1em;
 }
 </style>
