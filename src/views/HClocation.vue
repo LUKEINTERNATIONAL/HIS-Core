2a7aea70c15d7836e70182dfe3027e95051a19b4
Andrew Mfune <andiemfune@gmail.com>
2021-11-15 16:28:48 +0200

    Added next button for location scanning.
    Refactored code to include location api method

--- src/views/HClocation.vue
+++ src/views/HClocation.vue
@@ -5,75 +5,105 @@
         <ion-title>Scan work-station location</ion-title>
       </ion-toolbar>
     </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <barcode-scan></barcode-scan>
+    <ion-content>
+      <barcode-input 
+        class="his-card"
+        @onValue="onbarcodeText" 
+        @onScan="onScan">
+      </barcode-input>
     </ion-content>
-    <HisNavFooter @onCancel="gotoMain()" :showNext="false" @onFinish="gotoHome"></HisNavFooter>
+    <ion-footer> 
+      <ion-toolbar color="dark"> 
+        <ion-button 
+          color="danger" 
+          size="large" 
+          router-link="/login"> 
+          Cancel
+        </ion-button>
+        <ion-button
+          slot="end"
+          color="success"
+          size="large"
+          @click="searchLocation"
+          v-if="barcodeText"
+        > 
+          Next 
+        </ion-button>
+      </ion-toolbar>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script lang="ts">
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/vue';
-import { defineComponent } from 'vue';
-import BarcodeScan from '@/components/BarcodeScan.vue';
-import HisNavFooter from "@/components/HisNavFooter.vue";
+import { defineComponent, ref } from 'vue';
+import BarcodeInput from "@/components/BarcodeInput.vue"
+import {toastWarning} from "@/utils/Alerts"
+import router from '@/router/index';
+import { Service } from "@/services/service"
+import { isEmpty } from 'lodash';
+import {
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonButton,
+  IonFooter
+} from '@ionic/vue';
 
 export default defineComponent({
   name: 'HC location',
   components: {
-    HisNavFooter,
+    BarcodeInput,
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
     IonToolbar,
-    "barcode-scan": BarcodeScan
+    IonButton,
+    IonFooter
   },
-  methods: {
-    gotoHome(){
-      this.$router.push({path: "/"})
-    },
-    gotoMain() {
-      this.$router.push({path: "/login"})
+  setup() {
+    const barcodeText = ref('')
+
+    async function searchLocation() {
+      if (!barcodeText.value.includes('$')) {
+        barcodeText.value += '$'
+      }
+      const response = await Service.getJson(`locations/${barcodeText.value}`)
+      if (isEmpty(response)) {
+        barcodeText.value = ''
+        toastWarning("Invalid location")
+      }else {
+        const data = response
+        sessionStorage.userLocation = data.name;
+        router.push("/");
+      }
+    }
+
+    function onbarcodeText(t: string) {
+      barcodeText.value = t
+    }
+
+    async function onScan(t: string) {
+      barcodeText.value = t
+      await searchLocation()
+    }
+
+    return {
+      onbarcodeText,
+      onScan,
+      searchLocation,
+      barcodeText
     }
   }
-});
+})
 </script>
 
 <style scoped>
-.container {
+.his-card {
   margin: auto;
-  /* text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 20%;
-  transform: translateY(-50%);
-  background-color: #F0F0F0;
-  border-style: solid;
-  border-width: 1px;
-  border-radius: 9px;
-  height: 20%;
-  margin: 3%; */
+  width: 95%;
+  margin-top: 5%;
 }
-
-.loc-footer {
-    height: 10% !important;
-    background-color: #333 !important;
-}
-
-.footer-btns {
-  margin-left: 1%;
-  margin-top: 1%;
-  height: 8vh;
-}
-
- @media only screen and (max-width: 900px) {
-   #container {
-     top: 20%;
-     height: 15%;
-   }
- }
-
 </style>
