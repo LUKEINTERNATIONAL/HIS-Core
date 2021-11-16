e08adeaea5080eeabd725aa70aa5c69b0db87441
Daniel Justin <danieljmuleke@gmail.com>
2021-11-16 12:11:40 +0200

    Implemented presenting complaints program activity

---
+++ src/apps/OPD/views/encounters/Complaints.vue
@@ -0,0 +1,66 @@
+<template>
+  <his-standard-form :cancelDestinationPath="cancelDestination" :fields="fields" :onFinishAction="onSubmit"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HisStandardForm from "@/components/Forms/TouchScreenForm.vue";
+import EncounterMixinVue from '@/apps/ART/views/encounters/EncounterMixin.vue';
+import { PatientComplaintsService } from "@/apps/OPD/services/patient_complaints_service";
+import Validation from '@/components/Forms/validations/StandardValidations';
+import { Field, Option } from '@/components/Forms/FieldInterface';
+import { FieldType } from '@/components/Forms/BaseFormElements';
+
+export default defineComponent({
+  components: { HisStandardForm },
+  mixins: [EncounterMixinVue],
+  data: () => ({
+    complaintsService: {} as any,
+    complaints: [] as Array<any>,
+    complaintsGroup: [] as Array<any>
+  }),
+  watch: {
+    ready: {
+      async handler(isReady: boolean) {
+        if(isReady){
+          this.complaintsService = new PatientComplaintsService(this.patientID, this.providerID)
+          this.complaintsGroup = await PatientComplaintsService.getComplaintsList('Presenting complaint group')
+          this.fields = this.getFields()
+        }
+      },
+      immediate: true,
+      deep: true
+    }
+  },
+  methods: {
+    async onSubmit(_: any, computedData: any){   
+      await this.complaintsService.createEncounter()
+      await this.complaintsService.createEncounter()
+      
+      const data = await Promise.all(computedData.complaints)     
+      await this.complaintsService.saveObservationList(data)
+
+      this.nextTask()        
+    },
+    getFields(): Array<Field>{
+      return [
+        {
+          id: 'complaints',
+          helpText: 'Presenting Complaints',
+          type: FieldType.TT_COMPLAINTS_PICKER,
+          validation: (data: any) => Validation.required(data),
+          computedValue: (options: Option[]) => {
+            const obs = options.map(option => {
+              const parentObs = this.complaintsService.buildValueCoded('Presenting complaint', option.other.parent)
+              const childObs = this.complaintsService.buildValueCodedFromConceptId(option.other.parent, option.other.concept_id)
+             return [parentObs, childObs]
+            })
+            return obs.flat(1)
+          },
+        },
+      ]
+    }
+  }
+})
+</script>
+
