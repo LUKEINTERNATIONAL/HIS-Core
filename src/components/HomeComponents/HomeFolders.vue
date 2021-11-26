c5066f19d47418ef95c8459e839c72bbac448110
Andrew Mfune <andiemfune@gmail.com>
2021-11-26 10:30:33 +0200

    Fixed home folders breakpoints

--- src/components/HomeComponents/HomeFolders.vue
+++ src/components/HomeComponents/HomeFolders.vue
@@ -12,7 +12,7 @@
             <ion-col 
                 v-for="(item, index) in viewableItems" 
                 :key="index"
-                size="4"
+                size-lg="4"
                 size-sm="12"
                 v-show="canShowItem(item)"
                 >
