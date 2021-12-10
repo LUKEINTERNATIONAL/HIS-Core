154a0bcbc96598017561a34fc2a76a6d61d1b4de
Andrew Mfune <andiemfune@gmail.com>
2021-12-10 12:37:46 +0200

    Redirect to mastercard when the view button is clicked

--- src/apps/ART/views/reports/inconsistencies/DataCleaning.vue
+++ src/apps/ART/views/reports/inconsistencies/DataCleaning.vue
@@ -83,7 +83,7 @@ export default defineComponent({
     },
     masterCardBtn(patientId: number) {
         return table.tdBtn("View", () =>
-            this.$router.push(`/patient/dashboard/${patientId}`)
+            this.$router.push(`/art/mastercard/${patientId}`)
         )
     },
     getDefaultIndicatorColumns(customHeaders=[] as Array<any>){
