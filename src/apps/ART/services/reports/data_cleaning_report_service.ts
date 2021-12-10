45f0816d4a1068e2656ef5ddd97988ba430fad5f
Andrew Mfune <andiemfune@gmail.com>
2021-12-10 11:07:00 +0200

    Added endpoint for retrieving enrolled on art before birth

--- src/apps/ART/services/reports/data_cleaning_report_service.ts
+++ src/apps/ART/services/reports/data_cleaning_report_service.ts
@@ -3,6 +3,7 @@ import { ArtReportService } from "./art_report_service";
 export enum CtIndicator {
     DobMoreThanEnrolledDate = 'DOB MORE THAN DATE ENROLLED',
     ClientsWithEncountersAfterDeath = 'CLIENTS WITH ENCOUNTERS AFTER DECLARED DEAD',
+    DobMoreThanDateEnrolled = 'DOB MORE THAN DATE ENROLLED',
     DateEnrolledLessThanEarliestStartDate = 'DATE ENROLLED LESS THAN EARLIEST START DATE',
     MalesWithFemaleObs = 'MALE CLIENTS WITH FEMALE OBS',
     PrescriptionWithoutDispensation = 'PRESCRIPTION WITHOUT DISPENSATION',
@@ -21,5 +22,9 @@ export class DataCleaningReportService extends ArtReportService {
         return this.getReport('art_data_cleaning_tools', { 
             'report_name' : indicator 
         })
+    }
+
+    getEnrolledOnArtBeforeBirth() {
+        return ArtReportService.getJson('enrolled_on_art_before_birth')
     }
 }
