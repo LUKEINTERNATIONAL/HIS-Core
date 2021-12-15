19dce53d228b03dc37a0151ea8eee2ca3aecc7ea
Andrew Mfune <andiemfune@gmail.com>
2021-12-15 20:08:52 +0200

    Added maternal status api

--- src/apps/ART/services/reports/viral_load_report.ts
+++ src/apps/ART/services/reports/viral_load_report.ts
@@ -1,3 +1,4 @@
+import Url from "@/utils/Url";
 import { ArtReportService } from "./art_report_service";
 
 export class ViralLoadReportService extends ArtReportService {
@@ -9,5 +10,16 @@ export class ViralLoadReportService extends ArtReportService {
         return this.getReport(`programs/${this.programID}/reports/viral_load_coverage`, {
             'rebuild_outcomes': true
         })
+    }
+
+    getMaternalStatus(patientIds: number[]) {
+        const params = Url.parameterizeObjToString({
+            'start_date': this.startDate,
+            'end_date': this.endDate,
+            'date': this.date,
+            'program_id': this.programID,
+            'report_definition': 'pepfar'
+        })
+        return ArtReportService.postJson('vl_maternal_status', patientIds)
     }
 }
