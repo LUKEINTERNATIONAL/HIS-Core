6fbe6b632c32656fd30805f1ae8431a5674346a2
Andrew Mfune <andiemfune@gmail.com>
2021-12-22 07:02:48 +0200

    Importing form element directly to maintain a white list of supported ones

--- src/components/Forms/SIngleTouchField.vue
+++ src/components/Forms/SIngleTouchField.vue
@@ -1,40 +1,44 @@
 <template>
-    <ion-header>
-        <ion-toolbar>
-            <ion-title class="ion-text-center"> 
-                {{ helpText }}
-            </ion-title>
-        </ion-toolbar>
-    </ion-header>
-    <ion-content> 
-        <keep-alive>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title class="ion-text-center"> 
+                    {{ currentField.helpText }}
+                </ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content> 
             <component
-            :key="currentField.id"
-            v-bind:is="currentField.type"
-            :config="currentField.config"
-            :options="currentField.options"
-            :clear="valueClearIndex"
-            :fdata="formData"
-            :cdata="computedFormData"
-            :onValue="currentField.onValue"
-            :defaultValue="currentField.defaultValue"
-            @onValue="onFieldValue"
-            />
-        </keep-alive>
-    </ion-content>
-    <ion-footer> 
-        <ion-toolbar color="light">
-            <ion-button :disabled="disableBtn" @click="onClose" slot="start" color="danger" size="large"> 
-                Close
-            </ion-button>
-            <ion-button :disabled="disableBtn" @click="onClear" slot="end" color="warning" size="large"> 
-                Clear
-            </ion-button>
-            <ion-button :disabled="disableBtn" @click="onDone" slot="end" color="success" size="large"> 
-                Done
-            </ion-button>
-        </ion-toolbar>
-    </ion-footer>
+                :key="currentField.id"
+                :is="currentField.type"
+                :currentFieldig="currentField.currentFieldig"
+                :options="currentField.options"
+                :preset="currentField.preset"
+                :clear="valueClearIndex"
+                :fdata="formData"
+                :cdata="computedFormData"
+                :activationState="state"
+                :onValue="currentField.onValue"
+                :defaultValue="currentField.defaultValue"
+                :onValueUpdate="currentField.onValueUpdate"
+                @onValue="onFieldValue"
+            >
+            </component>
+        </ion-content>
+        <ion-footer> 
+            <ion-toolbar color="light">
+                <ion-button :disabled="disableBtn" @click="onClose" slot="start" color="danger" size="large"> 
+                    Close
+                </ion-button>
+                <ion-button :disabled="disableBtn" @click="onClear" slot="end" color="warning" size="large"> 
+                    Clear
+                </ion-button>
+                <ion-button :disabled="disableBtn" @click="onDone" slot="end" color="success" size="large"> 
+                    Done
+                </ion-button>
+            </ion-toolbar>
+        </ion-footer>
+    </ion-page>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
@@ -46,21 +50,46 @@ import {
     IonHeader,
     IonTitle,
     IonButton,
+    IonPage,
+    IonFooter,
     modalController
 } from "@ionic/vue"
-
+import BarcodeInput from "@/components/FormElements/HisBarcodeInput.vue"
+import SingleSelect from "@/components/FormElements/HisSelect.vue";
+import SingleSelectCards from "@/components/FormElements/HisCardSelector.vue";
+import MultipleSelect from "@/components/FormElements/HisMultipleSelect.vue";
+import TextInput from "@/components/FormElements/HisTextInput.vue"
+import NumberInput from "@/components/FormElements/HisNumberInput.vue"
+import MonthlyDays from "@/components/FormElements/HisMonthlyDays.vue"
+import YesNo from "@/components/FormElements/YesNoSelect.vue"
+import MultiYesNo from "@/components/FormElements/MultiYesNoSelect.vue"
+import IPAddressInput from "@/components/FormElements/HisIPAddress.vue"
+import DateInput from "@/components/FormElements/HisDateInput.vue"
 export default defineComponent({
+    name: 'SingleFieldTouchForm',
     components: {
-        IonToolbar,
+        IonPage,
         IonContent,
+        IonFooter,
+        IonToolbar,
+        IonButton,
         IonHeader,
         IonTitle,
-        IonButton
+        TextInput,
+        SingleSelect,
+        MultipleSelect,
+        NumberInput,
+        MonthlyDays,
+        BarcodeInput,
+        YesNo,
+        MultiYesNo,
+        SingleSelectCards,
+        DateInput,
+        IPAddressInput
     },
     props: {
         dismissType: {
-            type: Object as PropType<'modal' | 'redirect'>,
-            default: 'modal'
+            type: String
         },
         onFinish: {
            type: Function,
@@ -71,18 +100,13 @@ export default defineComponent({
             required: true
         }
     },
-    computed: {
-        helpText(): string {
-            return this.currentField?.helpText
-        }
-    },
     data: () => ({
         valueClearIndex: 0 as number,
         formData: {} as any,
         computedFormData: {} as any,
         state: "" as string,
         value: null as Option | Option[] | null,
-        disableBtn: false as boolean
+        disableBtn: false as boolean,
     }),
     methods: {
         onClose() {
