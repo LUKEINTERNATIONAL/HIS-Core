c64936a5f78b6e4f4e14aedc097c33dbec6db690
Daniel Justin <danieljmuleke@gmail.com>
2022-01-25 13:23:41 +0200

    merge fixes

--- src/apps/OPD/app.ts
+++ src/apps/OPD/app.ts
@@ -9,6 +9,7 @@ import PatientAlerts from "@/services/patient_alerts"
 import { Observation } from '@/interfaces/observation';
 import { OrderService } from '@/services/order_service';
 import { RelationshipService } from '@/services/relationship_service';
+import { Order } from '@/interfaces/order';
 
 async function onRegisterPatient(patientId: number){
   const program = new PatientProgramService(patientId)
@@ -36,16 +37,25 @@ async function formatPatientProgramSummary(data: any) {
 }
 
 async function getPatientDashboardAlerts(patient: any): Promise<GeneralDataInterface[]>{
-  const sideEffects: Observation[] 
-  = await PatientAlerts.alertSideEffects(
-      patient.getID()
-  )
+  const sideEffects: Observation[] = await PatientAlerts.alertSideEffects(patient.getID())
+  const bmi = await patient.getBMI()
   return [
-      {
-          label: "Side effects",
-          value: `${sideEffects.length}`,
-      }
+    {
+      label: "Side effects",
+      value: `${sideEffects.length}`,
+    },
+    {
+      label: "Patient BMI is",
+      value: `${bmi.result}`
+    }
   ]
+}
+
+function orderToString(order: Order) {
+  const test = order.tests[0];
+  const result = test.result[0];
+  const status = OrderService.isHighViralLoadResult(result) ? '(<b style="color: #eb445a;">High</b>)' : ''
+  return `${test.name} ${result.value_modifier}${result.value} ${status}`;
 }
 
 function confirmationSummary(patient: any, program: any) {
@@ -62,9 +72,9 @@ function confirmationSummary(patient: any, program: any) {
       await OrderService.getOrders(patient.getID())
         .then((orders) => {
           const VLOrders = OrderService.getViralLoadOrders(orders);
-          VLOrders.forEach((element) => {
+          VLOrders.forEach((order) => {
             data.push({
-              value: OrderService.formatOrders(element),
+              value: orderToString(order),
               label: ``,
             });
           });
