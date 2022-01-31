c3588a869032ef6296382522b11535da26dbdd28
Dominic Kasanga <dominickasanga@gmail.com>
2022-01-31 19:56:05 +0200

    added two clmn template

--- src/components/FormElements/HisSummary.vue
+++ src/components/FormElements/HisSummary.vue
@@ -1,22 +1,7 @@
 <template>
     <view-port>
         <div class="view-port-content"> 
-            <ion-list>
-                <ion-row>
-                    <ion-col width-50>
-                        <ion-item v-for="(item, index) in leftLs" :key="index">
-                            <ion-label> {{item.label}} </ion-label>
-                            <ion-label class="lb-value" slot="end"> {{item.value}} </ion-label>
-                            </ion-item>
-                        </ion-col>
-                    <ion-col width-50>
-                        <ion-item v-for="(item, index) in rightLs" :key="index">
-                            <ion-label> {{item.label}} </ion-label>
-                            <ion-label class="lb-value" slot="end"> {{item.value}} </ion-label>
-                        </ion-item>
-                    </ion-col>
-                </ion-row>
-            </ion-list>
+            <list :listData="listData"></list>
         </div>
   </view-port>
 </template>
@@ -25,18 +10,12 @@ import { defineComponent } from 'vue'
 import { Option } from "@/components/Forms/FieldInterface"
 import ViewPort from "@/components/DataViews/ViewPort.vue"
 import FieldMixinVue from './FieldMixin.vue'
-import {
-    IonList,
-    IonLabel,
-    IonItem,
-} from "@ionic/vue"
+import list from '@/components/DataViews/TwoColmunList.vue'
 
 export default defineComponent({
     components: { 
         ViewPort,
-        IonList,
-        IonLabel,
-        IonItem,
+        list
     },
     mixins: [FieldMixinVue],
     data: () => ({
@@ -46,24 +25,10 @@ export default defineComponent({
         this.$emit('onFieldActivated', this)
         this.listData = this.options(this.fdata, this.cdata)
     },
-    computed: {
-        leftLs(): any {
-        return this.listData.filter( (element, index) => index < 11)
-        },
-        rightLs(): any {
-        return this.listData.filter( (element, index) => index > 11)
-        }
-    },
 })
 </script>
 <style scoped>
     .view-port-content {
         height: 100%;
-    }
-    .lb-value {
-        font-weight: bold;
-    }
-    .item .sc-ion-label-md-h {
-        white-space: normal;
     }
 </style>
