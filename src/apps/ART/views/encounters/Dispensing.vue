f062dcec0f3c43aa9b4d0b5a11f0b9c698d3e43a
Andrew Mfune <andiemfune@gmail.com>
2022-02-01 11:54:20 +0200

    undo drug management checks

--- src/apps/ART/views/encounters/Dispensing.vue
+++ src/apps/ART/views/encounters/Dispensing.vue
@@ -10,7 +10,6 @@ import { DispensationService } from "@/apps/ART/services/dispensation_service"
 import { isEmpty } from 'lodash'
 import EncounterMixinVue from '../../../../views/EncounterMixin.vue'
 import HisDate from "@/utils/Date"
-import ART_PROP from "@/apps/ART/art_global_props"
 
 export default defineComponent({
     mixins: [EncounterMixinVue],
@@ -22,9 +21,9 @@ export default defineComponent({
         patient: {
             async handler(patient: any){
                 this.dispensation = new DispensationService(patient.getID(), this.providerID)
+
                 await this.dispensation.loadCurrentDrugOrder()
                 await this.dispensation.loadDrugHistory()
-                this.drugManagementEnabled = await ART_PROP.drugManagementEnabled()
                 this.fields = this.getFields()
             },
             deep: true
@@ -143,7 +142,6 @@ export default defineComponent({
                         return false
                     },
                     config: {
-                        drugManagementEnabled: this.drugManagementEnabled,
                         medicationHistory: this.buildMedicationHistory(),
                         toolbarInfo: [
                             { label: 'Name', value: this.patient.getFullName() },
