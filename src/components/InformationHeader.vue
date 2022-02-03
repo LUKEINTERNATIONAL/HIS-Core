1ee24e89440b7156a8a347de6be3b3cb0eadb630
Daniel Justin <danieljmuleke@gmail.com>
2022-02-03 13:42:57 +0200

    Refactored to use multicolumn view container

--- src/components/InformationHeader.vue
+++ src/components/InformationHeader.vue
@@ -1,35 +1,28 @@
 <template>
   <div style="padding: .1rem;">
-    <ion-grid>
-      <ion-row style="border: 1px solid #a5a5a599">
-        <ion-col v-for="(item, index) in computedItems" :key="index" :size="grid[numberOfColumns]">
-          <ion-list>
-            <ion-item v-for="option in item" :key="option.label" style="padding: 0 !important; margin: 0 !important;">
-              <div :style="{width: '100%', display: 'flex', justifyContent: 'space-between', fontSize: '11px', margin: 0, padding: 0}">
-                <span>{{ option.label }}: </span>
-                <span v-if="option.other && option.other.editable" @click="onClick(option)">
-                  <a><b>{{ option.value }}</b></a>
-                </span>
-                <span v-else><b>{{ option.value || 'N/A'}}</b></span>
-              </div>
-            </ion-item>
-          </ion-list>
-        </ion-col>
-      </ion-row>
-    </ion-grid>
+    <multi-column-view :items="items" :numberOfColumns="numberOfColumns" v-slot="{entries}">
+      <ion-list>
+        <ion-item v-for="option in entries" :key="option.label" style="padding: 0 !important; margin: 0 !important;">
+          <div :style="{width: '100%', display: 'flex', justifyContent: 'space-between', fontSize: '11px', margin: 0, padding: 0}">
+            <span>{{ option.label }}: </span>
+            <span v-if="option.other && option.other.editable" @click="onClick(option)">
+              <a><b>{{ option.value }}</b></a>
+            </span>
+            <span v-else><b>{{ option.value || 'N/A'}}</b></span>
+          </div>
+        </ion-item>
+      </ion-list>
+    </multi-column-view>
   </div>
 </template>
 <script lang="ts">
-import { computed, defineComponent, PropType } from "vue";
+import { defineComponent, PropType } from "vue";
 import { Option } from "@/components/Forms/FieldInterface";
-import { IonGrid, IonRow, IonCol } from "@ionic/vue";
-import { chunk } from "lodash";
+import MultiColumnView from "@/components/containers/MultiColumnView.vue";
 export default defineComponent({
-  name: "HisResultCard",
+  name: "InformationHeader",
   components: {
-    IonGrid,
-    IonRow,
-    IonCol,
+    MultiColumnView
   },
   props: {
     icon: {
@@ -46,10 +39,6 @@ export default defineComponent({
   },
   emits: ['update', 'addGuardian'],
   setup (props, { emit }) {
-    const grid: Record<number, number> = {1:12, 2:6, 3:4, 4:3}
-    const computedItems = computed(() => 
-      chunk(props.items, Math.ceil(props.items.length / props.numberOfColumns))
-    )
     const onClick = (entry: Option) => {
       if (entry.other.category === 'demographics') {
         return emit('update', entry.other.attribute)
@@ -58,9 +47,7 @@ export default defineComponent({
     }
 
     return {
-      computedItems,
       onClick,
-      grid,
     }
   } 
 });
